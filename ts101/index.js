c6e6d934cfa31f482177abf5bc51907bce61ad8f
suraj-jayarajan <suraj.pj.209@gmail.com>
2025-12-22 08:54:51 +0530

    Added Ts101

---
+++ ts101/index.js
@@ -0,0 +1,8 @@
+/* ==== Js code ==== */
+var ageJs = 25;
+var messageJs = "John Doe";
+console.log("Name: ".concat(messageJs, ", Age: ").concat(ageJs));
+/* ==== Ts code ==== */
+var age = 25;
+var message = "John Doe";
+console.log("Name: ".concat(message, ", Age: ").concat(age));
---
+++ ts101/index.ts
@@ -0,0 +1,11 @@
+/* ==== Js code ==== */
+let ageJs = 25;
+let messageJs = "John Doe";
+console.log(`Name: ${messageJs}, Age: ${ageJs}`);
+
+/* ==== Ts code ==== */
+let age: number = 25;
+let message: string = "John Doe";
+console.log(`Name: ${message}, Age: ${age}`);
+
+
